fa136192bf3243612e0b5f99372888ae4a367ee3
Didier Muñiz Estevez <didier.m@bnn.mx>
2020-02-18 11:14:24 -0600

    Initial commit

---
+++ src/js/functions.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    $('#carrusel-estar').owlCarousel({
+        autoplay: false,
+        dots: true,
+        nav: true,
+        items:1,
+        loop: false,
+        
+        // responsive: {
+        //     0: {
+        //         nav: false,
+        //     },
+        
+        //     996: {
+        //         nav: true,
+        //     }
+        // }
+    });
+
+
+    $('#carrusel-estigmas').owlCarousel({
+        autoplay: false,
+        dots: true,
+        nav: true,
+        items: 1,
+        loop: false,
+        center: true
+
+    });
+
+
+    $('#carrusel-recorrido').owlCarousel({
+        autoplay: false,
+        dots: true,
+        nav: true,
+        items: 1,
+        loop: false,
+        center: true
+
+    });
+    
+});
+
+    
+
