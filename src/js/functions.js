a340f37de87b7b65fad5a11cf37f92417956d6b8
Didier Muñiz Estevez <didier.m@bnn.mx>
2020-02-20 17:42:47 -0600

    btn directorio

--- src/js/functions.js
+++ src/js/functions.js
@@ -6,20 +6,39 @@ $(document).ready(function () {
         $(this).toggleClass("nav-on");
     });
 
-
-    $("#navbarNav li").on("click", function () {
-        var nameLi = $(this).attr("class").replace("li-", "home--");
-            nameSection = $("."+nameLi).offset().top - 100;
+    
+    $("#navbarNav li").not(".li-video").on("click", function () {
+        var nameLi = $(this).attr("class").replace("li-", "home--"),
+            nameSection = $("." + nameLi).offset().top - 100;
         
-        $("html, body").animate({ scrollTop: nameSection }, 800);
+        
+        $("html, body").animate({ scrollTop: nameSection }, 1500);
         
         $(".header--navegacion .btn").removeClass("nav-on");
         $(".navbar-collapse").removeClass("show");
         
         $("#navbarNav li").removeClass("activo");
         $(this).addClass("activo");
+        
     });
 
+    $(".btn-doctors").on("click", function () {
+        var videoS = $(".home--busqueda").offset().top - 100;
+
+        $("html, body").animate({ scrollTop: videoS }, 1500);
+
+        $(this).addClass("on-doc");
+
+        setTimeout(function () {
+            $(".btn-doctors").removeClass("on-doc");
+        }, 2000);
+    });
+
+    
+
+    if ($(".li-video").hasClass("d-none")){
+        $(".header--logo").addClass("l-m");
+    }
 
     // CARRUSEL ESTAR
 
@@ -31,7 +50,7 @@ $(document).ready(function () {
             0: {
                 items: 1,
                 dots: true,
-                onDragged: callback
+                onChanged: callback
             },
         
             996: {
@@ -42,8 +61,9 @@ $(document).ready(function () {
         }
     });
 
+
     function callback(event) {
-        if ($('#carrusel-estar').find(".owl-item:nth-child(2)").hasClass("active")) {
+        if ($('#carrusel-estar').find(".owl-item:nth-child(1)").hasClass("active")) {
             $("#carrusel-estar").addClass("carrusel-estar-2");
         }
 
@@ -102,24 +122,30 @@ $(document).ready(function () {
 
 
     $(".home--busqueda--form select").on("change", function () {
-        $(".home--busqueda .btn-on").parent().attr({ "href": $(this).val(),"target":"_blank" });
+        $(".home--busqueda .btn-on").parent().attr({ "href": $(this).val(), "target": "_blank" });
+        $(".alert").fadeOut();
     });
 
     $(".home--busqueda .btn-on").on("click", function () {
 
         
         if ($(".home--busqueda--form select").val() == null) {
-            alert("Elige un estado ");
+            // alert("Elige un estado ");
+            $(".alert").fadeIn();
         }
 
         else {
+            
 
-            if ( $(".checkbox[checked]").length == 2 ) {
+            if ($(".checkbox[checked]").length == 2) {
+                $(".alert").fadeOut();
                 return true;
             }
     
             else{
-                alert("Acepta nuestros Términos y Aviso de Privacidad");
+                
+                $(".alert").fadeIn();
+                $(".alert").text("Acepta nuestros Términos y Aviso de Privacidad");
                 return false;
             }
         }
